f03af426f5e164a7edf3623e3b7f38f540abc68a
gusals <gusals6586@gmail.com>
2023-03-16 14:42:24 +0900

    init

---
+++ src/main/java/com/auction/usedauction/WebConfig.java
@@ -0,0 +1,16 @@
+package com.auction.usedauction;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://bkkang1.github.io", "https://usedauction.shop")
+                .allowCredentials(true);
+    }
+}
---
+++ src/main/java/com/auction/usedauction/test/ApiTestController.java
@@ -0,0 +1,55 @@
+package com.auction.usedauction.test;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class ApiTestController {
+    @GetMapping
+    public ResponseEntity a(HttpServletRequest request, HttpServletResponse response) {
+        request.getSession().setAttribute("ss", "111");
+
+        log.info("session id={}",request.getSession().getId());
+        ResponseCookie cookie= ResponseCookie.from("JSESSIONID",request.getSession().getId())
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+//                .maxAge(0)
+                .path("/")
+                .domain("bkkang1.github.io")
+                .build();
+        return ResponseEntity.ok()
+//                .header(HttpHeaders.SET_COOKIE,cookie.toString())
+                .body(new Hello("hello"));
+
+    }
+
+    @GetMapping("/AAA")
+    public String result(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("ss") == null) {
+            return "fail";
+        }
+        return "ok";
+    }
+
+    @Data
+    static class Hello {
+        private String name;
+
+        public Hello(String name) {
+            this.name = name;
+        }
+    }
+
+}
