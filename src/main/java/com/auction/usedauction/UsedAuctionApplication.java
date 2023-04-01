363522ad11bff5dc50a3b7e99c45f5dd344914af
HO <love47024702@naver.com>
2023-04-01 17:09:29 +0900

    Chore: LocalDateTime 한국 시간으로 설정

--- src/main/java/com/auction/usedauction/UsedAuctionApplication.java
+++ src/main/java/com/auction/usedauction/UsedAuctionApplication.java
@@ -1,16 +1,21 @@
 package com.auction.usedauction;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.TimeZone;
 import java.util.UUID;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class UsedAuctionApplication {
 
 	public static void main(String[] args) {
@@ -20,5 +25,11 @@ public class UsedAuctionApplication {
 	@Bean
 	public AuditorAware<String> auditorProvider() { // 수정 예정
 		return () -> Optional.of(UUID.randomUUID().toString());
+	}
+
+	@PostConstruct
+	public void setTimeZone() { // LocalDateTime 한국 시간 기준으로
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("LocalDateTime.now()={}",LocalDateTime.now());
 	}
 }
