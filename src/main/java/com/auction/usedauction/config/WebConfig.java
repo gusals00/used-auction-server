c25f0347b5aa05ab84d5f043993c5f711f2384bc
HO <love47024702@naver.com>
2023-04-01 22:26:10 +0900

    Rename: WebConfig.java 위치 변경

--- src/main/java/com/auction/usedauction/config/WebConfig.java
+++ src/main/java/com/auction/usedauction/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.auction.usedauction;
+package com.auction.usedauction.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
