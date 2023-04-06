63fe2a0412418158bb7265a45da421aaec920430
HO <love47024702@naver.com>
2023-04-06 22:02:52 +0900

    Feat: 상품 삭제 조건 추가

--- src/main/java/com/auction/usedauction/exception/error_code/ProductErrorCode.java
+++ src/main/java/com/auction/usedauction/exception/error_code/ProductErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ProductErrorCode implements ErrorCode {
 
 
-    PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST,"해당 상품을 찾을 수 없습니다.");
-
+    PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST,"해당 상품을 찾을 수 없습니다."),
+    PRODUCT_DELETE_FAIL(HttpStatus.BAD_REQUEST, "해당 상품을 삭제할 수 없습니다.");
     private final HttpStatus status;
     private final String message;
 }
