3ce1f02cf927f005586e12e07dddebfa09fd4212
HO <love47024702@naver.com>
2023-04-12 00:00:27 +0900

    Test: 질문 페이징 조회 테스트 추가

--- src/main/java/com/auction/usedauction/domain/BaseTimeEntity.java
+++ src/main/java/com/auction/usedauction/domain/BaseTimeEntity.java
@@ -21,4 +21,8 @@ public abstract class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+
+    public void changeCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
 }
