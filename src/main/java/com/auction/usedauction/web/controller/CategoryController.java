5d06040c1f12df992b8c2a44b2d44835bb5ebde6
HO <love47024702@naver.com>
2023-04-14 20:58:16 +0900

    Docs: 카테고리 리스트 조회 api 문서 작성

--- src/main/java/com/auction/usedauction/web/controller/CategoryController.java
+++ src/main/java/com/auction/usedauction/web/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.auction.usedauction.web.controller;
 import com.auction.usedauction.repository.CategoryRepository;
 import com.auction.usedauction.web.dto.CategoryRes;
 import com.auction.usedauction.web.dto.ResultRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,13 @@ import static java.util.stream.Collectors.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/categories")
+@Tag(name = "카테고리 컨트롤러", description = "카테고리 관련 api")
 public class CategoryController {
 
     private final CategoryRepository categoryRepository;
 
     @GetMapping
+    @Operation(summary = "카테고리 리스트 조회 메서드")
     public ResultRes<List<CategoryRes>> getCategoryList() {
         return new ResultRes<>(categoryRepository.findAll().stream()
                 .map(CategoryRes::new)
--- src/main/java/com/auction/usedauction/web/dto/CategoryRes.java
+++ src/main/java/com/auction/usedauction/web/dto/CategoryRes.java
@@ -1,6 +1,7 @@
 package com.auction.usedauction.web.dto;
 
 import com.auction.usedauction.domain.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CategoryRes {
 
+    @Schema(description = "카테고리 ID",example = "1")
     private Long categoryId;
+    @Schema(description = "카테고리 이름",example = "스포츠/레저")
     private String name;
 
     public CategoryRes(Category category) {
