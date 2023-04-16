159aa57ea8ef181b18cfb8ae6a8a4684e7633339
HO <love47024702@naver.com>
2023-04-16 23:16:13 +0900

    Feat: AuctionHistoryErrorCode 추가

--- src/main/java/com/auction/usedauction/exception/error_code/AuctionHistoryErrorCode.java
+++ src/main/java/com/auction/usedauction/exception/error_code/AuctionHistoryErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuctionHistoryErrorCode implements ErrorCode{
     AUCTION_HISTORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "입찰 기록이 없습니다."),
-    AUCTION_FAIL(HttpStatus.BAD_REQUEST, "입찰에 실패했습니다."),
+    NO_HIGHER_THAN_NOW_PRICE(HttpStatus.BAD_REQUEST, "입찰 가격이 현재 가격보다 작습니다."),
+    INVALID_PRICE_UNIT(HttpStatus.BAD_REQUEST, "단위 가격이 올바르지 않습니다."),
     NOT_BID_SELLER(HttpStatus.BAD_REQUEST, "판매자는 입찰이 불가능합니다."),
     NOT_BID_BUYER(HttpStatus.BAD_REQUEST, "연속 2번 입찰은 불가능합니다."),
 
