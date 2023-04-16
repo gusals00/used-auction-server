895cc0db00d58e3a259abe590b661587e6bedd71
HO <love47024702@naver.com>
2023-04-16 23:17:40 +0900

    Refactor: 입찰 기능 리팩토링

--- src/main/java/com/auction/usedauction/service/AuctionHistoryService.java
+++ src/main/java/com/auction/usedauction/service/AuctionHistoryService.java
@@ -8,7 +8,6 @@ import com.auction.usedauction.exception.error_code.UserErrorCode;
 import com.auction.usedauction.repository.auction.AuctionRepository;
 import com.auction.usedauction.repository.MemberRepository;
 import com.auction.usedauction.repository.auction_history.AuctionHistoryRepository;
-import com.auction.usedauction.repository.query.AuctionHistoryQueryRepository;
 import com.auction.usedauction.service.dto.AuctionBidResultDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +22,6 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class AuctionHistoryService {
 
-    private final AuctionHistoryQueryRepository auctionHistoryQueryRepository;
     private final AuctionRepository auctionRepository;
     private final AuctionHistoryRepository auctionHistoryRepository;
     private final MemberRepository memberRepository;
@@ -31,29 +29,9 @@ public class AuctionHistoryService {
     @Transactional
     public AuctionBidResultDTO biddingAuction(Long auctionId, int bidPrice, String loginId) {
 
-        // 경매중 인지 확인,
+        // 경매중 인지 확인
         Auction findAuction = auctionRepository.findBidAuctionByAuctionIdWithFetchJoin(auctionId)
                 .orElseThrow(() -> new CustomException(AuctionErrorCode.AUCTION_NOT_BIDDING));
-
-        // 최근 입찰자 조회
-        Optional<String> latestMemberLoginId = auctionHistoryRepository.findLatestBidMemberLoginId(auctionId);
-
-        // 첫 입찰일 경우
-        if (latestMemberLoginId.isEmpty()) {
-            // 현재 금액 <= 입찰 금액인지, 입찰 단위가 맞는지
-            if (findAuction.getNowPrice() > bidPrice || (bidPrice - findAuction.getNowPrice()) % findAuction.getPriceUnit() != 0) {
-                throw new CustomException(AuctionHistoryErrorCode.AUCTION_FAIL);
-            }
-        } else {// 첫 입,찰이 아닌 경우
-            // 현재 금액 < 입찰 금액, 입찰 단위가 맞는지
-            if (findAuction.getNowPrice() >= bidPrice || (bidPrice - findAuction.getNowPrice()) % findAuction.getPriceUnit() != 0) {
-                throw new CustomException(AuctionHistoryErrorCode.AUCTION_FAIL);
-            }
-            // 최근 입찰자와 현재 입찰자가 다른지
-            if (latestMemberLoginId.get().equals(loginId)) {
-                throw new CustomException(AuctionHistoryErrorCode.NOT_BID_BUYER);
-            }
-        }
 
         //판매자는 입찰 불가능
         if (findAuction.getProduct().getMember().getLoginId().equals(loginId)) {
@@ -64,6 +42,31 @@ public class AuctionHistoryService {
         Member member = memberRepository.findOneByLoginIdAndStatus(loginId, MemberStatus.EXIST)
                 .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
 
+        // 최근 입찰자 조회
+        Optional<String> latestMemberLoginId = auctionHistoryRepository.findLatestBidMemberLoginId(auctionId);
+
+        // 첫 입찰일 경우
+        if (latestMemberLoginId.isEmpty()) {
+            // 현재 금액 <= 입찰 금액인지
+            if (findAuction.getNowPrice() > bidPrice) {
+                throw new CustomException(AuctionHistoryErrorCode.NO_HIGHER_THAN_NOW_PRICE);
+            }
+            // 입찰 단위가 맞는지
+            validPriceUnit(bidPrice,findAuction);
+
+        } else {// 첫 입찰이 아닌 경우
+            // 현재 금액 < 입찰 금액
+            if (findAuction.getNowPrice() >= bidPrice) {
+                throw new CustomException(AuctionHistoryErrorCode.NO_HIGHER_THAN_NOW_PRICE);
+            }
+            // 입찰 단위가 맞는지
+            validPriceUnit(bidPrice,findAuction);
+
+            // 최근 입찰자와 현재 입찰자가 다른지
+            if (latestMemberLoginId.get().equals(loginId)) {
+                throw new CustomException(AuctionHistoryErrorCode.NOT_BID_BUYER);
+            }
+        }
 
         // 현재 금액 변경
         findAuction.increaseNowPrice(bidPrice);
@@ -73,6 +76,12 @@ public class AuctionHistoryService {
         return new AuctionBidResultDTO(bidPrice, findAuction.getProduct().getId(), auctionHistory.getId());
     }
 
+    private void validPriceUnit(int bidPrice, Auction auction) {
+        if ((bidPrice - auction.getNowPrice()) % auction.getPriceUnit() != 0) {
+            throw new CustomException(AuctionHistoryErrorCode.INVALID_PRICE_UNIT);
+        }
+    }
+
     private AuctionHistory createAuctionHistory(Auction auction, int bidPrice, Member member) {
         return AuctionHistory.builder()
                 .auction(auction)
@@ -80,4 +89,6 @@ public class AuctionHistoryService {
                 .bidPrice(bidPrice)
                 .build();
     }
+
+
 }
