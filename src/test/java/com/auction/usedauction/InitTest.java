1dd9a6fd0cdb1378c7981d9b2cfd20389e87c923
HO <love47024702@naver.com>
2023-04-17 15:17:23 +0900

    Refactor: beforeEach 한번에 통합

---
+++ src/test/java/com/auction/usedauction/InitTest.java
@@ -0,0 +1,19 @@
+package com.auction.usedauction;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Profile(value = {"test"})
+public class InitTest {
+
+    private final InitTestService initTestService;
+
+    @PostConstruct
+    public void init() {
+        initTestService.initTest();
+    }
+}
