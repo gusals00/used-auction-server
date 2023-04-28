a93557a7f0a03b15589f25afa59a9f326da5dd60
HO <love47024702@naver.com>
2023-04-28 16:13:17 +0900

    Feat: SseEmitterUtils 추가

--- src/main/java/com/auction/usedauction/util/SseEmitterUtils.java
+++ src/main/java/com/auction/usedauction/util/SseEmitterUtils.java
@@ -6,7 +6,8 @@ import java.util.UUID;
 
 public class SseEmitterUtils {
 
-    // sseType-UUID-productId( 로그인 아이디를 모르는 경우)
+    // sseType-UUID-productId(로그인 아이디를 모르는 경우)
+    // CHAT_LIST-sefwefd.wws2-2sdf-1
     public static String createSseEmitterId(SseType sseType, Long productId) {
         return new StringBuilder()
                 .append(sseType.toString())
@@ -17,7 +18,8 @@ public class SseEmitterUtils {
                 .toString();
     }
 
-    // sseType-loginId-productId(로그인 아이디를 아는 경우)
+    // sseType-loginId-productId
+    // CHAT_LIST-sefwefd.wws2-2sdf-1
     public static String createSseEmitterId(SseType sseType, String loginId, Long productId) {
         return new StringBuilder()
                 .append(sseType.toString())
@@ -25,6 +27,17 @@ public class SseEmitterUtils {
                 .append(loginId)
                 .append("-")
                 .append(productId)
+                .toString();
+    }
+
+    // sseType-loginId-productId(productId를 모르는 경우)
+    public static String createSseEmitterId(SseType sseType, String loginId) {
+        return new StringBuilder()
+                .append(sseType.toString())
+                .append("-")
+                .append(loginId)
+                .append("-")
+                .append(UUID.randomUUID().toString())
                 .toString();
     }
 
