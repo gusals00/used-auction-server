641459aaca18ed1751789e58e6979812d86bb04d
HO <love47024702@naver.com>
2023-04-28 16:32:43 +0900

    Chore: async 수정

--- src/main/java/com/auction/usedauction/config/AsyncConfig.java
+++ src/main/java/com/auction/usedauction/config/AsyncConfig.java
@@ -15,9 +15,9 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10); //기본 실행 대기하는 Thread 수
-        executor.setMaxPoolSize(100); //동시 동작하는 최대 Thread 수
-        executor.setQueueCapacity(30); // 최대 쓰레드 수 보다 많은 요청이 올 경우 queue에 저장 가능한 요청 개수
+        executor.setCorePoolSize(40); //한번에 실행 가능한 Thread 수
+        executor.setMaxPoolSize(100); //최대 Thread pool 크기
+        executor.setQueueCapacity(70); // CorePoolSize넘어서는 요청이 오면 queue에 쌓을 수 있는 최대 요청 수
         executor.setThreadNamePrefix("THREAD-ASYNC-"); // 생성되는 Thread 접두사
         executor.initialize();
         return executor;
