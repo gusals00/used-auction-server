52b46be2fd5651d8b7d680f3ee2d5c38bf12094c
gusals <gusals6586@gmail.com>
2023-05-01 15:21:52 +0900

    Refactor: 채팅메세지 쿼리 수정

--- src/main/java/com/auction/usedauction/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/auction/usedauction/repository/chat/ChatMessageRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     @Modifying(clearAutomatically = true)
-    @Query(value = "update chat_message c join member m on c.chat_message_id = m.member_id set c.read_or_not = true where m.login_id not like :loginId and c.chat_room_id = :roomId ", nativeQuery = true)
+    @Query(value = "update chat_message c join member m on c.member_id = m.member_id set c.read_or_not = true where m.login_id not like :loginId and c.chat_room_id = :roomId and c.read_or_not = false", nativeQuery = true)
     int updateMessages(@Param("loginId") String loginId, @Param("roomId") Long roomId);
 
     @Query(value = "select c from ChatMessage c join fetch c.member where c.chatRoom.id = :chatRoomId order by c.createdDate desc",
