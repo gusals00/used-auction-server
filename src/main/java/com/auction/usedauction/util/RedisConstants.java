d495d1d750c4790e148d26b8999958da22a4da7a
gusals <gusals6586@gmail.com>
2023-05-01 22:42:50 +0900

    Feat: RedisUtil, Redis상수 추가

---
+++ src/main/java/com/auction/usedauction/util/RedisConstants.java
@@ -0,0 +1,6 @@
+package com.auction.usedauction.util;
+
+public interface RedisConstants {
+    String REFRESH_TOKEN = "RefreshToken:";
+    String ROOM_LIST = "RoomList:";
+}
--- src/main/java/com/auction/usedauction/util/RedisUtil.java
+++ src/main/java/com/auction/usedauction/util/RedisUtil.java
@@ -5,7 +5,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -23,6 +25,23 @@ public class RedisUtil {
 
     public void setData(String key, String value, Long timeout, TimeUnit unit) {
         redisTemplate.opsForValue().set(key, value, timeout, unit);
+    }
+
+    public void addList(String key, String value) {
+        redisTemplate.opsForList().rightPush(key, value);
+    }
+
+    public void setList(String key, List<String> list, Long timeout, TimeUnit unit) {
+        for (String value : list) {
+            addList(key, value);
+        }
+        redisTemplate.expire(key, timeout, unit);
+    }
+
+    public List<String> getList(String key) {
+        Long size = redisTemplate.opsForList().size(key);
+        return redisTemplate.opsForList().range(key, 0, size).stream()
+                .map(o -> (String) o).collect(Collectors.toList());
     }
 
     public String getData(String key) {
