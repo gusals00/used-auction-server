c731439ef381856693577df52ad4857f44c0a7fb
HO <love47024702@naver.com>
2023-05-02 00:17:37 +0900

    Feat: 경매내역 상태 변경 추가

--- src/main/java/com/auction/usedauction/repository/auction_history/AuctionHistoryRepository.java
+++ src/main/java/com/auction/usedauction/repository/auction_history/AuctionHistoryRepository.java
@@ -2,7 +2,11 @@ package com.auction.usedauction.repository.auction_history;
 
 import com.auction.usedauction.domain.Auction;
 import com.auction.usedauction.domain.AuctionHistory;
+import com.auction.usedauction.domain.AuctionHistoryStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +14,10 @@ import java.util.List;
 public interface AuctionHistoryRepository extends JpaRepository<AuctionHistory, Long>, AuctionHistoryRepositoryCustom {
 
     List<AuctionHistory> findAllByAuction(Auction auction);
+
     int countByAuction(Auction auction);
+
+    @Modifying
+    @Query(value = "update auction_history ah set ah.status = :status where ah.auction_history_id in (:auction_history_ids)", nativeQuery = true)
+    int updateAuctionHistoryStatus(@Param("status") AuctionHistoryStatus auctionHistoryStatus, @Param("auction_history_ids") List<Long> auctionHistoryIdList);
 }
