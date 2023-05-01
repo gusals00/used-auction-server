88e08a9e951a838084343ae42f4096634b37c2d7
HO <love47024702@naver.com>
2023-05-02 00:15:33 +0900

    Feat: 경매 상태 변경 추가

--- src/main/java/com/auction/usedauction/repository/auction/AuctionRepository.java
+++ src/main/java/com/auction/usedauction/repository/auction/AuctionRepository.java
@@ -4,13 +4,21 @@ import com.auction.usedauction.domain.Auction;
 
 import com.auction.usedauction.domain.AuctionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface AuctionRepository extends JpaRepository<Auction, Long>, AuctionRepositoryCustom {
 
-    boolean existsAuctionByIdAndStatus(Long actionId,AuctionStatus auctionStatus);
+    boolean existsAuctionByIdAndStatus(Long actionId, AuctionStatus auctionStatus);
 
-    Optional<Auction> findAuctionByIdAndStatus(Long auctionId,AuctionStatus auctionStatus);
+    Optional<Auction> findAuctionByIdAndStatus(Long auctionId, AuctionStatus auctionStatus);
+
+    @Modifying
+    @Query(value = "update auction a set a.status = :status where a.auction_id in (:auction_ids)", nativeQuery = true)
+    int updateAuctionStatus(@Param("status") AuctionStatus auctionStatus, @Param("auction_ids") List<Long> auctionIdList);
 }
