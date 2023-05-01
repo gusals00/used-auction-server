9cad05d6a14c4fda26333de1c13b94da1acebbac
HO <love47024702@naver.com>
2023-05-02 00:16:54 +0900

    Feat: 경매별 입찰자 수,id 조회 추가

--- src/main/java/com/auction/usedauction/repository/query/AuctionHistoryQueryRepository.java
+++ src/main/java/com/auction/usedauction/repository/query/AuctionHistoryQueryRepository.java
@@ -1,15 +1,14 @@
 package com.auction.usedauction.repository.query;
 
 import com.auction.usedauction.domain.*;
-import com.auction.usedauction.repository.dto.QSellerAndBuyerIdDTO;
-import com.auction.usedauction.repository.dto.QSellerAndBuyerLoginIdDTO;
-import com.auction.usedauction.repository.dto.SellerAndBuyerIdDTO;
-import com.auction.usedauction.repository.dto.SellerAndBuyerLoginIdDTO;
+import com.auction.usedauction.repository.dto.*;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static com.auction.usedauction.domain.QAuction.auction;
@@ -30,10 +29,10 @@ public class AuctionHistoryQueryRepository {
         return Optional.ofNullable(
                 queryFactory.select(new QSellerAndBuyerIdDTO(seller.id, buyer.id))
                         .from(auctionHistory)
-                        .join(auctionHistory.member,buyer)
-                        .rightJoin(auctionHistory.auction,auction)
-                        .join(auction.product,product)
-                        .join(product.member,seller)
+                        .join(auctionHistory.member, buyer)
+                        .rightJoin(auctionHistory.auction, auction)
+                        .join(auction.product, product)
+                        .join(product.member, seller)
                         .where(auctionIdEq(auctionId))
                         .orderBy(auctionHistory.bidPrice.desc())
                         .fetchFirst()
@@ -48,18 +47,36 @@ public class AuctionHistoryQueryRepository {
         return Optional.ofNullable(
                 queryFactory.select(new QSellerAndBuyerLoginIdDTO(seller.loginId, buyer.loginId))
                         .from(auctionHistory)
-                        .join(auctionHistory.member,buyer)
-                        .rightJoin(auctionHistory.auction,auction)
-                        .join(auction.product,product)
-                        .join(product.member,seller)
+                        .join(auctionHistory.member, buyer)
+                        .rightJoin(auctionHistory.auction, auction)
+                        .join(auction.product, product)
+                        .join(product.member, seller)
                         .where(auctionIdEq(auctionId))
                         .orderBy(auctionHistory.bidPrice.desc())
                         .fetchFirst()
         );
     }
 
+    // 경매 상태, 시간 기준으로 auctionId, 입찰 수 리턴
+    public List<AuctionIdAndBidCountDTO> findIdAndBidCountListByStatusAndEndDate(AuctionStatus status, LocalDateTime date) {
+        return  queryFactory.select(new QAuctionIdAndBidCountDTO(auctionHistory.id.count(), auction.id))
+                .from(auctionHistory)
+                .rightJoin(auctionHistory.auction, auction)
+                .where(auctionStatusEq(status), afterThanAuctionEndDate(date))
+                .groupBy(auction.id)
+                .fetch();
+    }
+
+    private BooleanExpression auctionStatusEq(AuctionStatus auctionStatus) {
+        return auctionStatus != null ? auction.status.eq(auctionStatus) : null;
+    }
+
+    private BooleanExpression afterThanAuctionEndDate(LocalDateTime localDateTime) {
+        return localDateTime!=null ? auction.auctionEndDate.before(localDateTime) : null;
+    }
+
     private BooleanExpression auctionIdEq(Long auctionId) {
-        return auctionId != null ?auction.id.eq(auctionId) : null;
+        return auctionId != null ? auction.id.eq(auctionId) : null;
     }
 
 }
