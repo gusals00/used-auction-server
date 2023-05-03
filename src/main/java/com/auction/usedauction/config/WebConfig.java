f289049717c587417097262b782370dcba68c4ca
HO <love47024702@naver.com>
2023-05-03 11:47:27 +0900

    Feat: 경매 낙찰 확인 interceptor 적용

---
+++ src/main/java/com/auction/usedauction/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.auction.usedauction.config;
+
+import com.auction.usedauction.interceptor.AuctionEndCheckInterceptor;
+import com.auction.usedauction.repository.auction_end.AuctionEndRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuctionEndRepository auctionEndRepository;
+    private final MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuctionEndCheckInterceptor(auctionEndRepository, mappingJackson2HttpMessageConverter))
+                .order(1)
+                .addPathPatterns("/api/auctions/**");
+
+    }
+}
---
+++ src/main/java/com/auction/usedauction/interceptor/AuctionEndCheckInterceptor.java
@@ -0,0 +1,54 @@
+package com.auction.usedauction.interceptor;
+
+import com.auction.usedauction.exception.CustomException;
+import com.auction.usedauction.exception.error_code.AuctionErrorCode;
+import com.auction.usedauction.repository.auction_end.AuctionEndRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AuctionEndCheckInterceptor implements HandlerInterceptor {
+
+    private final AuctionEndRepository auctionEndRepository;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Map pathVariables = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        String pathVariable = (String) pathVariables.get("auctionId");
+        log.info("REQUEST URL = {}, pathVariable = {}", request.getRequestURL(), pathVariable);
+        Long auctionId = Long.valueOf(pathVariable);
+
+        LocalDateTime auctionEndDate = auctionEndRepository.findByAuctionId(auctionId);
+        if (auctionEndDate == null) {
+            log.info("정해진 기간 내에 경매 종료 시간이 해당되지 않는 auctionId 입니다.");
+            return true;
+        }
+        if (auctionEndDate.isBefore(LocalDateTime.now())) {
+            log.info("경매 종료 시간이 지났습니다.");
+            throw new CustomException(AuctionErrorCode.AUCTION_NOT_BIDDING);
+        }
+        return true;
+    }
+
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        Map pathVariables = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+//        String pathVariable = (String) pathVariables.get("auctionId");
+//        log.info("REQUEST URL = {}, pathVariable = {}", request.getRequestURL(), pathVariable);
+//        Long auctionId = Long.valueOf(pathVariable);
+//
+//        throw new CustomException(AuctionErrorCode.AUCTION_NOT_BIDDING);
+//        setException(request, AuctionErrorCode.AUCTION_NOT_BIDDING);
+//        return false;
+//
+//    }
+}
