949e59304aa057a48ec2e01e4fe62a2188cf61c1
HO <love47024702@naver.com>
2023-05-16 14:58:42 +0900

    Feat: S3 rollback Manager 추가

---
+++ src/main/java/com/auction/usedauction/util/BackUpCommand.java
@@ -0,0 +1,6 @@
+package com.auction.usedauction.util;
+
+public interface BackUpCommand {
+    String INSERT = "insert";
+    String DELETE = "delete";
+}
---
+++ src/main/java/com/auction/usedauction/util/BackUpData.java
@@ -0,0 +1,63 @@
+package com.auction.usedauction.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+
+@Slf4j
+public class BackUpData {
+
+    private Map<String, List<String>> backUp;
+    private int level;
+
+    public BackUpData() {
+        level = 0;
+        backUp = new HashMap<>();
+        backUp.put(BackUpCommand.INSERT, new ArrayList<>());
+        backUp.put(BackUpCommand.DELETE, new ArrayList<>());
+    }
+
+    public void insert(String backUpCommand, String path) {
+        backUp.get(backUpCommand).add(path);
+        log.info("insert backup data, command = {}, path = {}", backUpCommand, path);
+    }
+
+    public List<BackUpDTO> getBackUp(String backUpCommand) {
+        return backUp.get(backUpCommand).stream()
+                .map(path -> new BackUpDTO(backUpCommand, path))
+                .collect(toList());
+    }
+
+    public int getInsertSize() {
+        return backUp.get(BackUpCommand.INSERT).size();
+    }
+
+    public int getDeleteSize() {
+        return backUp.get(BackUpCommand.DELETE).size();
+    }
+
+    public void prevLevel() {
+        level -= 1;
+    }
+
+    public void nextLevel() {
+        level += 1;
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+    public void clean() {
+        backUp.clear();
+    }
+
+    public boolean isEmpty() {
+        return backUp.isEmpty();
+    }
+}
