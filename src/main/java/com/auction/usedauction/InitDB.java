573896f08811728cbe72edac9d94e71cdd1f8e22
HO <love47024702@naver.com>
2023-05-24 12:59:30 +0900

    Chore: 더미데이터 생성 sql procedure

--- src/main/java/com/auction/usedauction/InitDB.java
+++ src/main/java/com/auction/usedauction/InitDB.java
@@ -5,15 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 @Profile(value = {"local","production"})
 public class InitDB {
     private final InitDBService initDBService;
-
+    private final InitProcedure initProcedure;
     @PostConstruct
-    public void init() {
+    public void init() throws IOException {
         initDBService.init();
         initDBService.initScheduler();
+        // 더미 데이터 추가 프로시저 실행 함수
+//        initProcedure.initProcedure();
     }
 }
