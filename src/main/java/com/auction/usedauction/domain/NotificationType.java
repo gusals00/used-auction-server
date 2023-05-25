f2dc26a1c5791fdcd2b347ab8c57463097b93892
gusals <gusals6586@gmail.com>
2023-05-25 18:11:22 +0900

    Feat: 알림 entity 추가

---
+++ src/main/java/com/auction/usedauction/domain/NotificationType.java
@@ -0,0 +1,13 @@
+package com.auction.usedauction.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum NotificationType {
+    SELLER_TRANS_CONFIRM("판매자 거래확정"),
+    BUYER_TRANS_CONFIRM("구매자 거래확정");
+
+    private final String description;
+}
